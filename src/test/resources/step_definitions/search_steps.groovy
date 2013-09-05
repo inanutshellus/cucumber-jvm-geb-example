e2be4f5088307c9f9a273f6a3cccccbdd7a1d7e8
inanutshellus <blackbox@inanutshell.us>
2013-09-04 22:05:31 -0400

    Switching the default to use the page objects.

--- src/test/resources/step_definitions/search_steps.groovy
+++ src/test/resources/step_definitions/search_steps.groovy
@@ -5,29 +5,29 @@ this.metaClass.mixin(cucumber.api.groovy.EN)
 this.metaClass.mixin(geb.Browser)
 
 Given(~'I am on the Google search page') { ->
-	browser.go('http://www.google.com/')
-	
-	// to GoogleHomePage
-	// at GoogleHomePage
+	// browser.go('http://www.google.com/')
+	// at GoogleHomePage  
+        
+	to GoogleHomePage // <-- "to" implicitly does an "at" check,  
 }
 
 When(~'I search for "(.+)"') { searchTerm ->
-	browser.$('input', name: 'q').value(searchTerm)
-	browser.$('button', name: 'btnG').click()
+	//browser.$('input', name: 'q').value(searchTerm)
+	//browser.$('button', name: 'btnG').click()
 	 
-	// searchFor(searchTerm)
+	searchFor(searchTerm)
 }
 
 Then(~'I should see the results page') { ->
 	Thread.sleep(1000)
-	def title = browser.page.title
-	assert title.endsWith("Google Search") || title.endsWith("Căutare Google")
+	//def title = browser.page.title
+	//assert title.endsWith("Google Search")
 	
-	// at GoogleResultsPage
+	at GoogleResultsPage
 } 
 
 Then(~'The first link should be "(.+)"') { searchTerm ->
-	assert $('div#ires ol li')[0].find('a').text() == searchTerm
+	//assert $('div#ires ol li')[0].find('a').text() == searchTerm
 	 
-	// assertResultIs(1, searchTerm)
+	assertResultIs(1, searchTerm)
 }
